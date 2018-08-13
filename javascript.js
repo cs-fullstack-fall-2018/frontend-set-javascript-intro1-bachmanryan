a17db74a77871ee70044fb1316abee46f1974ab3
Ryan Bachman <jacksprat455@gmail.com>
2018-08-13 10:57:16 -0500

    Frontend JavaScript

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("changeTheHeadlineText").innerText = "This is the Head line now";
+
+document.getElementById("myNameAtTheEnd").innerText =;
+
+document.getElementById("kennButton").innerText = "Dis is the Button Now";
